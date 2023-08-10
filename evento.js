c5aef8ce737195c64a56b7e9f8aaad10588a4840
kt-soo <137802962+kt-soo@users.noreply.github.com>
2023-08-10 11:14:16 -0300

    js new

--- evento.js
+++ evento.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("boton").addEventListener("click", function(e) {
-        e.stopPropagation();
-        alert("Hola, soy el div");
+    // Obtén una referencia al botón
+    var boton = document.getElementById("myButton");
+
+    // Agrega un manejador de eventos al hacer clic en el botón
+    boton.addEventListener("click", function(e) {
+        e.stopPropagation(); // Detiene la propagación del evento
+        alert("¡Hiciste clic en el botón!");
+    });
+
+    // Agrega un manejador de eventos al hacer clic en el div
+    document.getElementById("container").addEventListener("click", function() {
+        alert("¡Clic en el div!");
     });
 });
